f8947af3532c0721bbf49f2addf8d9fe5dd288ed
Ravi Ray <106743740+rv10ravi@users.noreply.github.com>
2024-08-22 13:31:55 +0530

    Settings Page Updated

--- app/screens/SettingsScreen.tsx
+++ app/screens/SettingsScreen.tsx
@@ -9,11 +9,9 @@ import {
   Button,
   Alert,
   TouchableOpacity,
+  Image,
+  Modal,
 } from "react-native";
-import { Collapsible } from "@/components/Collapsible";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 import { auth, db } from "../../firebaseConfig";
 import { getDoc, setDoc, doc, deleteDoc } from "firebase/firestore";
 import {
@@ -34,21 +32,6 @@ type HomeScreenNavigationProp = StackNavigationProp<
   "Login"
 >;
 
-const settingsData = [
-  {
-    title: "Account",
-    items: ["Change Password", "Manage Subscriptions"],
-  },
-  {
-    title: "Privacy",
-    items: ["Blocked Users", "Activity Status", "Location Services"],
-  },
-  {
-    title: "Notifications",
-    items: ["Push Notifications", "Email Notifications"],
-  },
-];
-
 export default function ProfileAndSettingsScreen() {
   const navigation = useNavigation<HomeScreenNavigationProp>();
   const [profileData, setProfileData] = useState({
@@ -59,8 +42,10 @@ export default function ProfileAndSettingsScreen() {
 
   const [currentPassword, setCurrentPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
   const [deletePassword, setDeletePassword] = useState("");
   const [loading, setLoading] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -104,8 +89,19 @@ export default function ProfileAndSettingsScreen() {
       });
   };
 
-  const handleChangePassword = async () => {
+  const handlePasswordChange = async () => {
     const user = auth.currentUser;
+
+    if (!currentPassword || !newPassword || !confirmNewPassword) {
+      Alert.alert("Error", "Please fill out all fields.");
+      return;
+    }
+
+    if (newPassword !== confirmNewPassword) {
+      Alert.alert("Error", "New passwords do not match.");
+      return;
+    }
+
     if (user) {
       const credential = EmailAuthProvider.credential(
         user.email!,
@@ -117,6 +113,10 @@ export default function ProfileAndSettingsScreen() {
           updatePassword(user, newPassword)
             .then(() => {
               Alert.alert("Success", "Password updated successfully.");
+              setIsModalVisible(false);
+              setCurrentPassword("");
+              setNewPassword("");
+              setConfirmNewPassword("");
             })
             .catch((error) => {
               Alert.alert("Error", error.message);
@@ -162,142 +162,206 @@ export default function ProfileAndSettingsScreen() {
     }
   };
 
-  if (loading) {
-    return <Text>Loading...</Text>;
-  }
-
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <Ionicons size={310} name="settings-sharp" style={styles.headerImage} />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Profile and Settings</ThemedText>
-      </ThemedView>
-      <ScrollView contentContainerStyle={styles.contentContainer}>
-        <Collapsible title="Profile">
-          <View style={styles.profileItem}>
-            <ThemedText style={styles.profileTitle}>Name</ThemedText>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.profileSection}>
+        <Image
+          source={require("../../assets/images/user (3).png")}
+          style={styles.profileImage}
+        />
+        <Text style={styles.profileName}>{profileData.name}</Text>
+        <Text style={styles.profileEmail}>{profileData.email}</Text>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Profile</Text>
+        <View style={styles.profileItem}>
+          <Text style={styles.profileLabel}>Name</Text>
+          <TextInput
+            style={styles.profileInput}
+            value={profileData.name}
+            onChangeText={(text) =>
+              setProfileData({ ...profileData, name: text })
+            }
+          />
+        </View>
+        <View style={styles.profileItem}>
+          <Text style={styles.profileLabel}>Phone</Text>
+          <TextInput
+            style={styles.profileInput}
+            value={profileData.phone}
+            onChangeText={(text) =>
+              setProfileData({ ...profileData, phone: text })
+            }
+          />
+        </View>
+        <Button
+          title="Update Profile"
+          onPress={handleProfileUpdate}
+          color="#5bb262"
+        />
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Account</Text>
+        <TouchableOpacity
+          style={styles.option}
+          onPress={() => setIsModalVisible(true)}
+        >
+          <Ionicons name="lock-closed-outline" size={24} color="#FFF" />
+          <Text style={styles.optionText}>Change Password</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.option} onPress={handleSignOut}>
+          <Ionicons name="log-out-outline" size={24} color="#FFF" />
+          <Text style={styles.optionText}>Sign Out</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.option} onPress={handleDeleteAccount}>
+          <Ionicons name="trash-outline" size={24} color="#FF6060" />
+          <Text style={[styles.optionText, { color: "#FF6060" }]}>
+            Delete Account
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Change Password Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={() => {
+          setIsModalVisible(!isModalVisible);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Change Password</Text>
             <TextInput
-              style={styles.profileInput}
-              value={profileData.name}
-              onChangeText={(text) =>
-                setProfileData({ ...profileData, name: text })
-              }
+              style={styles.modalInput}
+              placeholder="Current Password"
+              secureTextEntry
+              value={currentPassword}
+              onChangeText={setCurrentPassword}
             />
-          </View>
-          <View style={styles.profileItem}>
-            <ThemedText style={styles.profileTitle}>Email</ThemedText>
             <TextInput
-              style={styles.profileInput}
-              value={profileData.email}
-              editable={false}
+              style={styles.modalInput}
+              placeholder="New Password"
+              secureTextEntry
+              value={newPassword}
+              onChangeText={setNewPassword}
             />
-          </View>
-          <View style={styles.profileItem}>
-            <ThemedText style={styles.profileTitle}>Phone</ThemedText>
             <TextInput
-              style={styles.profileInput}
-              value={profileData.phone}
-              onChangeText={(text) =>
-                setProfileData({ ...profileData, phone: text })
-              }
+              style={styles.modalInput}
+              placeholder="Confirm New Password"
+              secureTextEntry
+              value={confirmNewPassword}
+              onChangeText={setConfirmNewPassword}
             />
+            <View style={styles.modalButtons}>
+              <Button
+                title="Cancel"
+                onPress={() => setIsModalVisible(false)}
+                color="#808080"
+              />
+              <Button
+                title="Change Password"
+                onPress={handlePasswordChange}
+                color="#5bb262"
+              />
+            </View>
           </View>
-          <Button title="Update Profile" onPress={handleProfileUpdate} />
-        </Collapsible>
-        {settingsData.map((section, index) => (
-          <Collapsible key={index} title={section.title}>
-            {section.items.map((item, idx) => {
-              switch (item) {
-                case "Change Password":
-                  return (
-                    <View key={idx} style={styles.settingItem}>
-                      <Text>Current Password:</Text>
-                      <TextInput
-                        style={styles.profileInput}
-                        value={currentPassword}
-                        secureTextEntry={true}
-                        onChangeText={(text) => setCurrentPassword(text)}
-                      />
-                      <Text>New Password:</Text>
-                      <TextInput
-                        style={styles.profileInput}
-                        value={newPassword}
-                        secureTextEntry={true}
-                        onChangeText={(text) => setNewPassword(text)}
-                      />
-                      <Button
-                        title="Change Password"
-                        onPress={handleChangePassword}
-                      />
-                    </View>
-                  );
-                default:
-                  return (
-                    <TouchableOpacity key={idx}>
-                      <ThemedText style={styles.settingItem}>{item}</ThemedText>
-                    </TouchableOpacity>
-                  );
-              }
-            })}
-          </Collapsible>
-        ))}
-        <Button title="Sign Out" onPress={handleSignOut} color="#FF6060" />
-        <Collapsible title="Delete Account">
-          <View style={styles.settingItem}>
-            <Text>Password:</Text>
-            <TextInput
-              style={styles.profileInput}
-              value={deletePassword}
-              secureTextEntry={true}
-              onChangeText={(text) => setDeletePassword(text)}
-            />
-            <Button
-              title="Delete Account"
-              onPress={handleDeleteAccount}
-              color="#FF6060"
-            />
-          </View>
-        </Collapsible>
-      </ScrollView>
-    </ParallaxScrollView>
+        </View>
+      </Modal>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
-  },
-  titleContainer: {
-    flexDirection: "row",
-    gap: 8,
-  },
-  contentContainer: {
+  container: {
+    flexGrow: 1,
+    backgroundColor: "#121212",
     padding: 20,
+    paddingTop: 60,
+  },
+  profileSection: {
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10,
+  },
+  profileName: {
+    fontSize: 24,
+    color: "#FFF",
+    fontWeight: "bold",
+  },
+  profileEmail: {
+    fontSize: 16,
+    color: "#808080",
+  },
+  section: {
+    marginBottom: 30,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    color: "#FFF",
+    marginBottom: 10,
+  },
+  option: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 15,
+    borderBottomColor: "#808080",
+    borderBottomWidth: 1,
+  },
+  optionText: {
+    fontSize: 18,
+    color: "#FFF",
+    marginLeft: 15,
   },
   profileItem: {
     marginBottom: 20,
   },
-  profileTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
+  profileLabel: {
+    fontSize: 16,
+    color: "#808080",
+    marginBottom: 5,
   },
   profileInput: {
-    fontSize: 16,
-    color: "#666",
-    borderBottomWidth: 1,
-    borderBottomColor: "#E5E7EB",
-    padding: 5,
+    backgroundColor: "#1E1E1E",
+    color: "#FFF",
+    padding: 10,
+    borderRadius: 5,
   },
-  settingItem: {
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#E5E7EB",
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    width: "80%",
+    backgroundColor: "#1E1E1E",
+    borderRadius: 10,
+    padding: 20,
+  },
+  modalTitle: {
+    fontSize: 20,
+    color: "#FFF",
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  modalInput: {
+    backgroundColor: "#FFF",
+    color: "#000",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  modalButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
